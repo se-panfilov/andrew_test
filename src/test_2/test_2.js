90e3ab43c56ce751e05d2eab949eea31e09c3de1
Sergey Panfilov <se-panfilov@ya.ru>
2016-12-23 19:21:20 +0100

    test 2 improvements

---
+++ src/test_2/test_2.js
@@ -0,0 +1,32 @@
+const arr = [
+  'some',
+  'another',
+  'other',
+  'else',
+  'whatever'
+]
+
+function addStr (arr, str) {
+  if (arr.indexOf(str) === -1) throw 'Shit happens'
+  arr.push(str)
+
+  return arr
+}
+
+function deleteStr (arr, str) {
+  const index = arr.indexOf('else')
+  if (index > -1) arr.splice(index, 1)
+
+  return arr
+}
+
+function run (arr, cb) {
+  return arr.map(cb)
+}
+//Example: run(arr, v => v + '!')
+
+
+function getUrl () {
+  const url = document.getElementById('url_input').value
+  return $.get('https://crossorigin.me/' + url, null, data => alert("Data Loaded: " + data))
+}
